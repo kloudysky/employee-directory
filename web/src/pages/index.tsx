bbe102ff67e73e86eed22e07c607b3fa49f6349f
Adam Kamboj <kamboj.adam@gmail.com>
2021-07-26 15:30:58 -0400

    cleaned up imports

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,26 +1,8 @@
-import { Box, Grid } from "@chakra-ui/layout";
-import {
-  useDisclosure,
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  transition,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
-import { Formik, Form } from "formik";
+import { Grid } from "@chakra-ui/layout";
 import React from "react";
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { EmployeeCard } from "../components/EmployeeCard";
-import { FormModal } from "../components/FormModal";
 
 const Index = () => (
   <Container height="100vh">
