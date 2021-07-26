ec1ccd21adbcf3d64457e905e01e1760028a3711
Adam Kamboj <kamboj.adam@gmail.com>
2021-07-25 21:39:07 -0400

    added create employee functionality

--- server/src/resolvers/Employee.ts
+++ server/src/resolvers/Employee.ts
@@ -1,10 +1,31 @@
 import { Employee } from "./../entities/Employee";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 
+@InputType()
+class EmployeeInput {
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  title: string;
+
+  @Field()
+  photoUrl: string;
+}
 @Resolver()
 export class EmployeeResolver {
   @Query(() => [Employee])
   employees(): Promise<Employee[]> {
     return Employee.find();
+  }
+
+  @Mutation(() => Employee)
+  async createEmployee(
+    @Arg("options", () => EmployeeInput) options: EmployeeInput
+  ): Promise<Employee> {
+    return await Employee.create(options).save();
   }
 }
