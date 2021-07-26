8cb10fbeceb76a2d565b58c0648be2ed62eff3a6
Adam Kamboj <kamboj.adam@gmail.com>
2021-07-26 14:34:27 -0400

    swwapped table layout for grid layout

--- web/src/components/Container.tsx
+++ web/src/components/Container.tsx
@@ -3,14 +3,14 @@ import { Flex, useColorMode } from "@chakra-ui/react";
 export const Container = (props) => {
   const { colorMode } = useColorMode();
 
-  const bgColor = { light: "gray.50", dark: "gray.900" };
+  const bgColor = { light: "gray.50", dark: "gray.800" };
 
   const color = { light: "black", dark: "white" };
   return (
     <Flex
       direction="column"
       alignItems="center"
-      justifyContent="flex-start"
+      // justifyContent="flex-start"
       bg={bgColor[colorMode]}
       color={color[colorMode]}
       {...props}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,53 +1,30 @@
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-} from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/layout";
 import React from "react";
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 
 const Index = () => (
   <Container height="100vh">
-    <Table variant="striped" colorScheme="teal">
-      <TableCaption>Imperial to metric conversion factors</TableCaption>
-      <Thead>
-        <Tr>
-          <Th>To convert</Th>
-          <Th>into</Th>
-          <Th isNumeric>multiply by</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        <Tr>
-          <Td>inches</Td>
-          <Td>millimetres (mm)</Td>
-          <Td isNumeric>25.4</Td>
-        </Tr>
-        <Tr>
-          <Td>feet</Td>
-          <Td>centimetres (cm)</Td>
-          <Td isNumeric>30.48</Td>
-        </Tr>
-        <Tr>
-          <Td>yards</Td>
-          <Td>metres (m)</Td>
-          <Td isNumeric>0.91444</Td>
-        </Tr>
-      </Tbody>
-      <Tfoot>
-        <Tr>
-          <Th>To convert</Th>
-          <Th>into</Th>
-          <Th isNumeric>multiply by</Th>
-        </Tr>
-      </Tfoot>
-    </Table>
+    <Grid mt={10} templateColumns="repeat(5, 1fr)" gap={6}>
+      <Box
+        maxW="lg"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        h="100"
+      >
+        Employee
+      </Box>
+      <Box
+        maxW="lg"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        h="100"
+      >
+        Employee
+      </Box>
+    </Grid>
     <DarkModeSwitch />
   </Container>
 );
