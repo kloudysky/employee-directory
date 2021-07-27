5d2fa086793c60c8f7a48744c0441f60d0f91ea4
Adam Kamboj <kamboj.adam@gmail.com>
2021-07-26 21:48:30 -0400

    converted index.tsx to class component

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,14 +5,20 @@ import { CreateEmployeeModal } from "../components/CreateEmployeeModal";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { EmployeeCard } from "../components/EmployeeCard";
 
-const Index = () => (
-  <Container height="100vh">
-    <CreateEmployeeModal />
-    <Grid mt={10} templateColumns="repeat(5, 1fr)" gap={6}>
-      <EmployeeCard firstName="kloud" lastName="strife" />
-    </Grid>
-    <DarkModeSwitch />
-  </Container>
-);
+export interface IndexState {}
+
+class Index extends React.Component<IndexState> {
+  render() {
+    return (
+      <Container height="100vh">
+        <CreateEmployeeModal />
+        <Grid mt={10} templateColumns="repeat(5, 1fr)" gap={6}>
+          <EmployeeCard firstName="kloud" lastName="strife" />
+        </Grid>
+        <DarkModeSwitch />
+      </Container>
+    );
+  }
+}
 
 export default Index;
